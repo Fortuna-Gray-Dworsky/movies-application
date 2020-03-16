dd26c9a990d163a40761f98bd64ec1a8fa556fa3
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-03-16 14:11:51 -0500

    added add a movie function

--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@
 const $ = require('jquery');
 const {getMovies} = require('./api.js');
 const apiKey = require('./keys.js');
-const movieAPI = `http://www.omdbapi.com/?apikey=${apiKey}&t=star+wars`;        //working in syntax to search for title
+const apiURL = `http://www.omdbapi.com/?apikey=${apiKey}&t=star+wars`;
+//working in syntax to search for title
 
-fetch(movieAPI)
-    .then(response => response.json())
-    .then(response => {
-        console.log(response);
-    });
+
+//Function to grab movie data from API and push it as an object into db.json file
+// fetch(apiURL)
+//     .then(response => response.json())
+//     .then(response => {
+//         console.log(response);
+//         let userSearchMovie = {
+//             title: response.Title,
+//             rating: [response.Ratings[0], response.Ratings[1], response.Ratings[2]]
+//         };
+//         console.log(userSearchMovie);
+//
+//         //add eventlistener to 2 buttons, add to list and cancel
+//         //search db.json for duplicate movie title before adding to server
+//
+//         let options = {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify(userSearchMovie),
+//         };
+//         fetch('api/movies', options);
+//     });
 
 //Main Render
 function mainRender() {
@@ -21,3 +41,28 @@ function mainRender() {
 }
 
 mainRender();
+
+
+//New Movie Add
+$('.submitButton').click(function () {
+    //Store user-submitted title
+    let userTitle = $('#userTitle').val();
+    //Store user-submitted rating
+    let userRating = $('#userRating').val();
+
+    let userAddMovie = {
+        title: userTitle,
+        rating: userRating
+    };
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userAddMovie),
+    };
+    fetch('api/movies', options);
+    userTitle = "";
+    userRating = "";
+    mainRender();
+});
