82d26f28a316345e1a12f8ed1353cb3875f9ac9e
Austin Gray <Austin.m.gray210@gmail.com>
2020-03-16 13:20:40 -0500

    updated

--- src/index.js
+++ src/index.js
@@ -1,23 +1,83 @@
-// require style imports
+/**
+ * es6 modules and imports
+ */
 const $ = require('jquery');
+import sayHello from './hello';
+import {fetch} from "whatwg-fetch";
+sayHello('World');
+
+/**
+ * require style imports
+ */
+const omdbKey = require('./keys.js');
 const {getMovies} = require('./api.js');
-const apiKey = require('./keys.js');
-const movieAPI = `http://www.omdbapi.com/?apikey=${apiKey}&t=star+wars`;        //working in syntax to search for title
+const url = `http://www.omdbapi.com/?apikey=${omdbKey}&`;
 
-fetch(movieAPI)
-    .then(response => response.json())
-    .then(response => {
-        console.log(response);
-    });
 
-//Main Render
-function mainRender() {
-    $('.movie-info').html("");
-    getMovies().then((movies) => {
-        movies.forEach(({title, rating, id}) => {
-            $('.movie-info').append(`<div>id#${id} - ${title} - rating: ${rating}</div>`);
-        });
-    });
+
+
+let yearInput = "";
+const yearSearch = url + `y=${yearInput}`;
+let idInput = "";
+const idSearch = url + `i=${idInput}`;
+
+
+// Fetch user input
+
+$("#searchButton").click( function () {
+    // Title Search function
+    let titleInput = $('#userSearchValue').val();
+    let titleFixed = titleInput.split(" ").join("+");
+    let titleSearch = url + `&t=${titleFixed}` ;
+    console.log(titleSearch);
+    apiCall();
+
+    function apiCall() {
+        fetch(titleSearch)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                $("#mediaContainer").html("");
+                $("#mediaContainer").append(
+                    '<h3>' + data.Title + '</h3>' +
+                    '<img src="' + data.Poster + '"/>' +
+                    '<h3>' + "Released: " + data.Year + '</h3>' +
+                    '<h3>' + "Rated: " + data.Rated + '</h3>' +
+                    '<h3>' + "Release Date: " + data.Released + '</h3>' +
+                    '<h3>' + "Runtime: " + data.Runtime + '</h3>'
+                )
+            })
+    }
+
+
+});
+
+
+// Fetch movie information
+function apiCall() {
+    fetch(titleSearch)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+        })
 }
 
-mainRender();
+
+
+
+
+
+
+getMovies().then((movies) => {
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
+    });
+}).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
+});
