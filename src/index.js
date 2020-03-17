de4b1f17211d0263c726efd9aee6d20f9997138b
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-03-17 13:38:14 -0500

    finished render for pulling from db.json

--- src/index.js
+++ src/index.js
@@ -7,15 +7,8 @@ $(document).ready(function () {
     const {getMovies} = require('./api.js');
     const url = `http://www.omdbapi.com/?apikey=${omdbKey}&`;
     let movieTitleList = [];
-    let allMovieList = [];
 
-// let yearInput = "";
-// const yearSearch = url + `y=${yearInput}`;
-// let idInput = "";
-// const idSearch = url + `i=${idInput}`;
-
-
-// Checks db.json for movie titles
+    // Checks db.json for movie titles
     function getMovieData() {
         fetch('./api/movies')
             .then(function (resp) {
@@ -23,7 +16,6 @@ $(document).ready(function () {
             })
             .then(function (data) {
                 data.forEach(function (movieInfo) {
-                    allMovieList.push(movieInfo);
                     movieTitleList.push(movieInfo.title);
                 })
             });
@@ -32,65 +24,63 @@ $(document).ready(function () {
 
     getMovieData();
 
-// Fetch user input
+
+    // User-submitted search: event-listener and potential API fetch
     $("#searchButton").click(function () {
 
-            // Convert title to readable string for fetch url
-            let titleInput = $('#userSearchValue').val();
-            let titleFixed = titleInput.split(" ").join("+");
-            let titleSearch = url + `&t=${titleFixed}`;
+        // Convert title to readable string for fetch url
+        let titleInput = $('#userSearchValue').val();
+        let titleFixed = titleInput.split(" ").join("+");
+        let titleSearch = url + `&t=${titleFixed}`;
 
-
-            function apiCall() {
-                if (titleInput === "") {
-                    // Check if input is empty
-                    $("#mediaContainer").html("");
-                    alert("No Movie Selected");
-                } else if (titleInput !== "") {
-                    allMovieList.forEach(function (movie) {
-                        if (((movie.title).toLowerCase()) === ((titleInput).toLowerCase())) {
-                            populateCardDB(movie);
-                        } else {
-                            // Function to grab movie data from API and push it as an object into db.json file
-                            fetch(titleSearch)
-                                .then(response => response.json())
-                                .then(response => {
-                                    let userSearchMovie = {
-                                        title: response.Title,
-                                        rating: [response.Ratings[0], response.Ratings[1], response.Ratings[2]],
-                                        poster: response.Poster,
-                                        ratings: response.Ratings,
-                                        type: response.Type
-                                    };
-                                    console.log(userSearchMovie);
-
-                                    //add eventlistener to 2 buttons, add to list and cancel
-                                    //search db.json for duplicate movie title before adding to server
-
-                                    let options = {
-                                        method: 'POST',
-                                        headers: {
-                                            'Content-Type': 'application/json',
-                                        },
-                                        body: JSON.stringify(userSearchMovie),
-                                    };
-                                    movieTitleList = [];
-                                    getMovieData();
-                                    fetch('api/movies', options);
-                                    populateCardDB();
-                                    populateCard(response);
-                                });
-                        }
+        // Render cards from either local server or API
+        function renderMovieCards() {
+            // Check if input is empty
+            if (titleInput === "") {
+                $("#mediaContainer").html("");
+                alert("No Movie Selected");
+            }
+            // Check if movie is in db.json
+            else if (movieTitleList.indexOf(titleInput) !== -1) {
+                fetch('./api/movies')
+                    .then(function (resp) {
+                        return resp.json();
                     })
-                }
+                    .then(function (data) {
+                        return populateCardDB(data[movieTitleList.indexOf(titleInput)]);
+                    });
+            }
+            // Function to grab movie data from API and push it as an object into db.json file
+            else {
+                fetch(titleSearch)
+                    .then(response => response.json())
+                    .then(response => {
+                        let userSearchMovie = {
+                            title: response.Title,
+                            rating: [response.Ratings[0], response.Ratings[1], response.Ratings[2]],
+                            poster: response.Poster,
+                            ratings: response.Ratings,
+                            type: response.Type
+                        };
+                        let options = {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify(userSearchMovie),
+                        };
+                        fetch('api/movies', options);
+                        populateCard(response);
+                    });
             }
 
-            apiCall();
         }
-    );
+
+        renderMovieCards();
+    });
 
 
-// Populates card with info.
+    // Render card with API information
     function populateCard(data) {
         $("#mediaContainer").html("").append(
             '<div class="card" style="width: 18rem;">' +
@@ -108,6 +98,7 @@ $(document).ready(function () {
         )
     }
 
+    // Render card with db.json information
     function populateCardDB(movie) {
         $("#mediaContainer").html("").append(
             '<div class="card" style="width: 18rem;">' +
@@ -125,55 +116,29 @@ $(document).ready(function () {
         )
     }
 
-
-// // Populates card with info.
-// function populateCard(data) {
-//     $("#mediaContainer").html("").append(
-//         '<div class="card" style="width: 18rem;">' +
-//         '<img src="' + data.Poster + '" class="card-img-top" alt="' + data.Title + '"/>' +
-//         '<div class="card-body">' +
-//         '<div class="row">' +
-//         '<h5 class="card-title col-6 text-left">' + data.Title + '</h5>' +
-//         '<a href="#" class="col-6 text-right">More Info</a>' +
-//         '</div>' +
-//         '<p class="card-text text-center">Rated: ' + data.Rated + '</p>' +
-//         '<p class="card-text text-center">Released: ' + data.Released + '</p>' +
-//         '<p class="card-text text-center">Runtime: ' + data.Runtime + '</p>' +
-//         '</div>' +
-//         '</div>'
-//     )
-// }
-
-// $('.submitButton').click(addMovie());
-
-
-    function addMovie(data) {
-        let userAddMovie = {
-            title: data.Title,
-            rating: data.Rated,     // IE PG-13
-            poster: data.Poster,
-            ratings: [              // IE MetaCritic 7.7
-                data.Ratings[0],
-                data.Ratings[1],
-                data.Ratings[2]
-            ],
-            type: data.Type,
-        };
-        let options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(userAddMovie),
-        };
-        fetch('api/movies', options);
-    }
+    // // User-submitted addition to movies - adds data to db.json
+    // $('.addMovieButton').click(addMovie());
+    //
+    // function addMovie() {
+    //     let userAddMovie = {
+    //         title: `${#userTitle}`,
+    //         rating: data.rated,
+    //     };
+    //     let options = {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(userAddMovie),
+    //     };
+    //     fetch('api/movies', options);
+    // }
 
 
 // function mainRender() {
 //     getMovies().then((movies) => {
 //         console.log('Here are all the movies:');
-//         movies.forEach(({title, rating}) => {
+//         movies.forEach(({title, ratin}) => {
 //             populateCard(title, rating);
 //         });
 //     }).catch((error) => {
@@ -208,4 +173,5 @@ $(document).ready(function () {
 // });
 
 
-});
+})
+;
